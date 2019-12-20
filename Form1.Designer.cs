be4c593134175f21741452f32f9a813b8bc6ad98
Mitchell Bink <1331221bink@zuyd.nl>
2019-12-20 19:55:23 +0100

    added all kind of stuff

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.getSwitchesInfo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.putLightOn = new System.Windows.Forms.Button();
+            this.putLightOff = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // getSwitchesInfo
+            // 
+            this.getSwitchesInfo.Location = new System.Drawing.Point(98, 147);
+            this.getSwitchesInfo.Name = "getSwitchesInfo";
+            this.getSwitchesInfo.Size = new System.Drawing.Size(159, 58);
+            this.getSwitchesInfo.TabIndex = 0;
+            this.getSwitchesInfo.Text = "Get info switches";
+            this.getSwitchesInfo.UseVisualStyleBackColor = true;
+            this.getSwitchesInfo.Click += new System.EventHandler(this.GetSwitchesInfo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 589);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // putLightOn
+            // 
+            this.putLightOn.Location = new System.Drawing.Point(98, 211);
+            this.putLightOn.Name = "putLightOn";
+            this.putLightOn.Size = new System.Drawing.Size(159, 58);
+            this.putLightOn.TabIndex = 2;
+            this.putLightOn.Text = "Zet mijn lampje aan";
+            this.putLightOn.UseVisualStyleBackColor = true;
+            this.putLightOn.Click += new System.EventHandler(this.PutLightOn_Click);
+            // 
+            // putLightOff
+            // 
+            this.putLightOff.Location = new System.Drawing.Point(98, 292);
+            this.putLightOff.Name = "putLightOff";
+            this.putLightOff.Size = new System.Drawing.Size(159, 58);
+            this.putLightOff.TabIndex = 3;
+            this.putLightOff.Text = "Zet mijn lampje uit";
+            this.putLightOff.UseVisualStyleBackColor = true;
+            this.putLightOff.Click += new System.EventHandler(this.PutLightOff_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(45, 392);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(212, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Ik wil de lamp aan hebben met Kerstmis";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(56, 480);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(400, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(968, 994);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ingang";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1904, 1041);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(1053, 969);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(37, 38);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.putLightOff);
+            this.Controls.Add(this.putLightOn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.getSwitchesInfo);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getSwitchesInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button putLightOn;
+        private System.Windows.Forms.Button putLightOff;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,347 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Globalization;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+
+        public void getData()
+        {
+            HttpWebRequest request = GetRequest("http://localhost:8080/json.htm?type=command&param=getlightswitches&username=dGVzdA==&password=dGVzdA==");
+            WebResponse webResponse = request.GetResponse();
+            var responseText = new StreamReader(webResponse.GetResponseStream()).ReadToEnd();
+            dynamic valueJson = JValue.Parse(responseText);
+            //JObject jobject = JObject.parse(responseText);
+            var resultOne = valueJson["result"][1]["idx"].ToString();
+            label1.Text = resultOne;
+        }
+
+        private HttpWebRequest GetRequest(string url, string httpMethod = "GET", bool allowAutoRedirect = true)
+        {
+            Uri uri = new Uri(url);
+            HttpWebRequest request = HttpWebRequest.Create(url) as HttpWebRequest;
+            request.UserAgent = @"Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko";
+
+            request.Timeout = Convert.ToInt32(new TimeSpan(0, 5, 0).TotalMilliseconds);
+            request.Method = httpMethod;
+            return request;
+        }
+
+        private void GetSwitchesInfo_Click(object sender, EventArgs e)
+        {
+            getData();
+        }
+
+        private void PutLightOff_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = GetRequest("http://localhost:8080/json.htm?type=command&param=switchlight&idx=2&switchcmd=Off&username=dGVzdA==&password=dGVzdA==");
+            WebResponse webResponse = request.GetResponse();
+            var responseText = new StreamReader(webResponse.GetResponseStream()).ReadToEnd();
+            dynamic valueJson = JToken.Parse(responseText);
+            var resultOne = valueJson["status"].ToString();
+            label1.Text = resultOne;
+        }
+
+        private void PutLightOn_Click(object sender, EventArgs e)
+        {
+            putLightsOn();
+            RunRoute();
+        }
+
+        private async void RunRoute()
+        {
+            for (int i = 0; i < 213; i++)
+            {
+                await ChangePictureBoxUp();
+            }
+            for (int i = 0; i < 512; i++)
+            {
+                await ChangePictureBoxLeft();
+            }
+            for (int i = 0; i < 165; i++)
+            {
+                await ChangePictureBoxUp();
+            }
+            for (int i = 0; i < 72; i++)
+            {
+                await ChangePictureBoxLeft();
+            }
+            for (int i = 0; i < 172; i++)
+            {
+                await ChangePictureBoxUp();
+            }
+            for (int i = 0; i < 72; i++)
+            {
+                await ChangePictureBoxRight();
+            }
+            for (int i = 0; i < 342; i++)
+            {
+                await ChangePictureBoxUp();
+            }
+            for (int i = 0; i < 168; i++)
+            {
+                await ChangePictureBoxRight();
+            }
+
+            for (int i = 0; i < 85; i++)
+            {
+                await ChangePictureBoxBottom();
+            }
+            for (int i = 0; i < 145; i++)
+            {
+                await ChangePictureBoxRight();
+            }
+            for (int i = 0; i < 85; i++)
+            {
+                await ChangePictureBoxUp();
+            }
+            for (int i = 0; i < 145; i++)
+            {
+                await ChangePictureBoxRight();
+            }
+            for (int i = 0; i < 220; i++)
+            {
+                await ChangePictureBoxBottom();
+            }
+            for (int i = 0; i < 295; i++)
+            {
+                await ChangePictureBoxLeft();
+            }
+            for (int i = 0; i < 70; i++)
+            {
+                await ChangePictureBoxBottom();
+            }
+            for (int i = 0; i < 70; i++)
+            {
+                await ChangePictureBoxRight();
+            }
+            for (int i = 0; i < 165; i++)
+            {
+                await ChangePictureBoxBottom();
+            }
+            for (int i = 0; i < 70; i++)
+            {
+                await ChangePictureBoxLeft();
+            }
+            for (int i = 0; i < 75; i++)
+            {
+                await ChangePictureBoxBottom();
+            }
+            for (int i = 0; i < 480; i++)
+            {
+                await ChangePictureBoxRight();
+            }
+            for (int i = 0; i < 245; i++)
+            {
+                await ChangePictureBoxUp();
+            }
+            for (int i = 0; i < 80; i++)
+            {
+                await ChangePictureBoxLeft();
+            }
+            for (int i = 0; i < 200; i++)
+            {
+                await ChangePictureBoxUp();
+            }
+            for (int i = 0; i < 80; i++)
+            {
+                await ChangePictureBoxRight();
+            }
+            for (int i = 0; i < 135; i++)
+            {
+                await ChangePictureBoxUp();
+            }
+            for (int i = 0; i < 235; i++)
+            {
+                await ChangePictureBoxRight();
+            }
+            for (int i = 0; i < 80; i++)
+            {
+                await ChangePictureBoxBottom();
+            }
+            for (int i = 0; i < 165; i++)
+            {
+                await ChangePictureBoxRight();
+            }
+            for (int i = 0; i < 80; i++)
+            {
+                await ChangePictureBoxUp();
+            }
+            for (int i = 0; i < 80; i++)
+            {
+                await ChangePictureBoxRight();
+            }//tot hier gedaan
+
+            for (int i = 0; i < 330; i++)
+            {
+                await ChangePictureBoxBottom();
+            }
+            for (int i = 0; i < 185; i++)
+            {
+                await ChangePictureBoxLeft();
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                await ChangePictureBoxUp();
+            }
+            for (int i = 0; i < 150; i++)
+            {
+                await ChangePictureBoxLeft();
+            }
+            for (int i = 0; i < 300; i++)
+            {
+                await ChangePictureBoxBottom();
+            }
+            for (int i = 0; i < 160; i++)
+            {
+                await ChangePictureBoxRight();
+            }
+            for (int i = 0; i < 90; i++)
+            {
+                await ChangePictureBoxUp();
+            }
+            for (int i = 0; i < 155; i++)
+            {
+                await ChangePictureBoxRight();
+            }
+
+            for (int i = 0; i < 285; i++)
+            {
+                await ChangePictureBoxBottom();
+            }
+            for (int i = 0; i < 70; i++)
+            {
+                await ChangePictureBoxLeft();
+            }
+            for (int i = 0; i < 85; i++)
+            {
+                await ChangePictureBoxBottom();
+            }
+            for (int i = 0; i < 180; i++)
+            {
+                await ChangePictureBoxLeft();
+            }
+
+            for (int i = 0; i < 85; i++)
+            {
+                await ChangePictureBoxUp();
+            }
+            for (int i = 0; i < 315; i++)
+            {
+                await ChangePictureBoxLeft();
+            }
+            for (int i = 0; i < 215; i++)
+            {
+                await ChangePictureBoxBottom();
+            }
+            for (int i = 0; i < 25; i++)
+            {
+                await ChangePictureBoxLeft();
+            }
+        }
+
+        private void putLightsOn()
+        {
+            HttpWebRequest request = GetRequest("http://localhost:8080/json.htm?type=command&param=switchlight&idx=24&switchcmd=On&username=dGVzdA==&password=dGVzdA==");
+            WebResponse webResponse = request.GetResponse();
+            var responseText = new StreamReader(webResponse.GetResponseStream()).ReadToEnd();
+            dynamic valueJson = JToken.Parse(responseText);
+            var resultOne = valueJson["status"].ToString();
+                label1.Text = resultOne;
+        }
+
+        private void putLightsOff()
+        {
+            HttpWebRequest request = GetRequest("http://localhost:8080/json.htm?type=command&param=switchlight&idx=24&switchcmd=Off&username=dGVzdA==&password=dGVzdA==");
+            WebResponse webResponse = request.GetResponse();
+            var responseText = new StreamReader(webResponse.GetResponseStream()).ReadToEnd();
+            dynamic valueJson = JToken.Parse(responseText);
+            var resultOne = valueJson["status"].ToString();
+            label1.Text = resultOne;
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                putLightsOn();
+
+                progressBar1.Value = 0;
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = 100;
+                progressBar1.Step = 10;
+
+                for (int i = 0; i < 100; i++)
+                {
+                    progressBar1.PerformStep();
+                }
+            }
+            else
+            {
+                putLightsOff();
+
+                progressBar1.Value = 100;
+
+                for (int i = 0; i < 100; i++)
+                {
+                    Thread.Sleep(5);
+                    progressBar1.Value -= 1;
+
+
+                }
+
+            }
+
+
+        }
+
+        private async Task ChangePictureBoxUp()
+        {
+            await Task.Delay(1);
+            pictureBox1.Top -= 1;
+        }
+
+        private async Task ChangePictureBoxLeft()
+        {
+            await Task.Delay(1);
+            pictureBox1.Left -= 1;
+        }
+
+        private async Task ChangePictureBoxBottom()
+        {
+            await Task.Delay(1);
+            pictureBox1.Top += 1;
+        }
+
+        private async Task ChangePictureBoxRight()
+        {
+            await Task.Delay(1);
+            pictureBox1.Left += 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
